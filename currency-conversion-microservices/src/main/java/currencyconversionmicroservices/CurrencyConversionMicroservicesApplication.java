220ec6e50dd0994bb6014d76ea424b24157813c3
tknikhil <nikhiltk02@gmail.com>
2022-04-16 11:19:49 +0530

    Add Currency Converion project

---
+++ currency-conversion-microservices/src/main/java/currencyconversionmicroservices/CurrencyConversionMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package currencyconversionmicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyConversionMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConversionMicroservicesApplication.class, args);
+	}
+
+}
