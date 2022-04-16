4f9af0dba3a2c9ffa8894dcba70b4ede978220e7
tknikhil <nikhiltk02@gmail.com>
2022-04-16 18:17:04 +0530

    circuit breaker feature of resilience4j - @Retry

---
+++ currency-exchange-microservices/src/main/java/currencyexchangemicroservices/controller/CircuitBreakerController.java
@@ -0,0 +1,29 @@
+package currencyexchangemicroservices.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import io.github.resilience4j.retry.annotation.Retry;
+
+@RestController
+public class CircuitBreakerController {
+	
+	private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
+	
+	@GetMapping("/sample-api")
+	@Retry(name ="sample-api",fallbackMethod = "hardCodedResponse")//by default it retry 3 times to get specific no. of retry goto application.properties
+	public String sampleApi() {
+		logger.info("Sample API call recieved");
+		//intentionally writing the code which give error
+		ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", String.class);
+		return forEntity.getBody();
+	}
+	
+	public String hardCodedResponse(Exception ex) {
+		return "Fallback Response!";
+	}
+}
