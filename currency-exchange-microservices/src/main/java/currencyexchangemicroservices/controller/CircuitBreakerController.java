fffdb00b8ce536005068505185d308114e47e3c7
tknikhil <nikhiltk02@gmail.com>
2022-04-16 18:28:34 +0530

    circuit breaker feature of resilience4j -@CircuitBreaker

--- currency-exchange-microservices/src/main/java/currencyexchangemicroservices/controller/CircuitBreakerController.java
+++ currency-exchange-microservices/src/main/java/currencyexchangemicroservices/controller/CircuitBreakerController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 
 @RestController
 public class CircuitBreakerController {
@@ -15,7 +15,8 @@ public class CircuitBreakerController {
 	private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
 	
 	@GetMapping("/sample-api")
-	@Retry(name ="sample-api",fallbackMethod = "hardCodedResponse")//by default it retry 3 times to get specific no. of retry goto application.properties
+	/* @Retry(name ="sample-api",fallbackMethod = "hardCodedResponse") *///by default it retry 3 times to get specific no. of retry goto application.properties
+	@CircuitBreaker(name ="sample-api",fallbackMethod = "hardCodedResponse")
 	public String sampleApi() {
 		logger.info("Sample API call recieved");
 		//intentionally writing the code which give error
